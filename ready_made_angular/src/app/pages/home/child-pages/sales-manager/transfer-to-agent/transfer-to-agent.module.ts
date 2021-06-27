4c55efef2fc8e2f735eb3ac88370596cdcc8030d
mounitbhandari <tataibhandari@gmail.com>
2021-06-27 22:58:46 +0530

    import form moule

--- ready_made_angular/src/app/pages/home/child-pages/sales-manager/transfer-to-agent/transfer-to-agent.module.ts
+++ ready_made_angular/src/app/pages/home/child-pages/sales-manager/transfer-to-agent/transfer-to-agent.module.ts
@@ -4,6 +4,9 @@ import { CommonModule } from '@angular/common';
 import { TransferToAgentRoutingModule } from './transfer-to-agent-routing.module';
 import { TransferToAgentComponent } from './transfer-to-agent.component';
 
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import { FormGroup } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
@@ -11,7 +14,18 @@ import { TransferToAgentComponent } from './transfer-to-agent.component';
   ],
   imports: [
     CommonModule,
-    TransferToAgentRoutingModule
+    TransferToAgentRoutingModule,
+    MaterialModule
   ]
 })
-export class TransferToAgentModule { }
+export class TransferToAgentModule {
+  transferForm: FormGroup;
+  constructor(){
+    this.transferForm = new FormGroup({
+    agent_id: new FormControl(null),
+    agent_name: new FormControl(null)
+  });
+}
+ }
+
+
