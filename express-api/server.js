a71ada05208eb2021f1c64221e0dbc4b788706cb
sukantahui <huisukanta77@gmail.com>
2021-07-02 19:03:06 +0530

    express modified

--- express-api/server.js
+++ express-api/server.js
@@ -1,6 +1,5 @@
 // https://www.youtube.com/watch?v=xn9ef5pod18
-const dotenv = require('dotenv');
-dotenv.config();
+const { PORT } = require('./config');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mysqlConnection = require("./connection");
@@ -10,6 +9,6 @@ var app = express();
 app.use(bodyParser.json());
 app.use("/people",PeopleRoutes);
 
-app.listen(process.env.PORT,()=>{
-    console.log(`Example app listening at http://localhost:${process.env.PORT}`)
+app.listen(PORT,()=>{
+    console.log(`Example app listening at http://localhost:${PORT}`)
 });
