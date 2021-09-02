141658621ecfe2315ba1f5157e2d1668ac0174b0
sukantahui <huisukanta77@gmail.com>
2021-09-02 14:10:54 +0530

    working

--- gold_api/app/Http/Controllers/ReportController.php
+++ gold_api/app/Http/Controllers/ReportController.php
@@ -7,12 +7,14 @@ use App\Models\AgentToCustomer;
 use App\Models\BillDetails;
 use App\Models\BillMaster;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ReportController extends ApiController
 {
     public function test($startDate,$endDate,$agentId){
 
-        $customers = AgentToCustomer::whereAgentId($agentId)->pluck('cust_id');
-        return $this->successResponse($customers);
+//        $customers = AgentToCustomer::whereAgentId($agentId)->pluck('cust_id');
+        $queries = DB::select("SELECT cust_id, get_customer_sale_qty_by_date(cust_id,'2021-01-01', '2021-08-30') from customer_master");
+        return $this->successResponse($queries);
     }
 }
