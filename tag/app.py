7cb162eb284d5ab70eb95d7571b8c80d2d9e2fab
sreeparnadas <sreeparnadas0675@gmail.com>
2021-09-22 23:13:11 +0530

    working on python tag search by job id

--- tag/app.py
+++ tag/app.py
@@ -12,43 +12,16 @@ import requests
 # pip3 install mysql-connector
 import mysql.connector
 
-from streamlit_ace import st_ace
 
+st.title("Tag")
+prevJobId = ""
 
+left,right = st.columns(2)
 
-# max_width_str = f"max-width: 1500px;"
-# st.markdown(
-# 	f"""
-# 		<style>
-# 			.reportview-container .main .block-container {{{max_width_str}}}
-# 		</style>    
-# 	""",
-# 	unsafe_allow_html=True
-# )
+jobId = left.text_input("Job ID")
+searchJobId = left.button("Search")
 
-
-
-
-st.set_page_config(layout="wide")
-
-# Spawn a new Ace editor
-content = st_ace()
-st.markdown('---')
-
-
-def my_widget(key):
-    st.subheader('Hello there!')
-    return st.button("Click me " + key)
-
-# This works in the main area
-clicked = my_widget("first")
-
-# And within an expander
-my_expander = st.expander("Expand", expanded=True)
-with my_expander:
-    clicked = my_widget("second")
-
-# AND in st.sidebar!
-with st.sidebar:
-    clicked = my_widget("third")
+if jobId != prevJobId:
+    prevJobId = jobId
+    st.write(jobId)
     
