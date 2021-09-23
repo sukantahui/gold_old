f10fae0735b15d0af16d9942dd5d5d4f589bfcf4
sukantahui <huisukanta77@gmail.com>
2021-09-23 18:21:47 +0530

    start_app created

---
+++ tag/start_app - Copy.bat
@@ -0,0 +1,3 @@
+set root=E:\Bitnami\wampstack-8.0.0-1\apache2\htdocs\gold_old\tag\.venv
+call %root%\Scripts\activate.bat
+call streamlit run "E:\Bitnami\wampstack-8.0.0-1\apache2\htdocs\gold_old\tag\app.py"
