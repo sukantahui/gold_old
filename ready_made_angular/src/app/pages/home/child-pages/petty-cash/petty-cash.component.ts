af1decd9895cb38de3e68a37fd04c80f2d669c5d
sukantahui <huisukanta77@gmail.com>
2021-11-04 13:41:52 +0530

    form created

--- ready_made_angular/src/app/pages/home/child-pages/petty-cash/petty-cash.component.ts
+++ ready_made_angular/src/app/pages/home/child-pages/petty-cash/petty-cash.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {formatDate} from '@angular/common';
 
 @Component({
   selector: 'app-petty-cash',
@@ -6,8 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./petty-cash.component.scss']
 })
 export class PettyCashComponent implements OnInit {
+  expenditureForm: FormGroup;
+  constructor() {
+    const now = new Date();
+    const currentSQLDate = formatDate(now, 'yyyy-MM-dd', 'en');
+    this.expenditureForm = new FormGroup({
+      id: new FormControl(null),
+      transaction_date: new FormControl(currentSQLDate, [Validators.required]),
+      ledger_id: new FormControl(null, [Validators.required]),
+      asset_id: new FormControl(1, [Validators.required]),           // purchase
+      voucher_number: new FormControl(null),
+      amount: new FormControl(0, [Validators.required]),
+      voucher_id: new FormControl(2, [Validators.required]),
+      particulars: new FormControl(null, [Validators.maxLength(255)]),
+      user_id: new FormControl(5, [Validators.required])
+    });
 
-  constructor() { }
+  }
 
   ngOnInit(): void {
   }
--- ready_made_angular/src/app/pages/home/child-pages/petty-cash/petty-cash.module.ts
+++ ready_made_angular/src/app/pages/home/child-pages/petty-cash/petty-cash.module.ts
@@ -3,15 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { PettyCashRoutingModule } from './petty-cash-routing.module';
 import { PettyCashComponent } from './petty-cash.component';
+import {MatCardModule} from '@angular/material/card';
+import {FlexModule} from '@angular/flex-layout';
 
 
 @NgModule({
   declarations: [
     PettyCashComponent
   ],
-  imports: [
-    CommonModule,
-    PettyCashRoutingModule
-  ]
+    imports: [
+        CommonModule,
+        PettyCashRoutingModule,
+        MatCardModule,
+        FlexModule
+    ]
 })
 export class PettyCashModule { }
