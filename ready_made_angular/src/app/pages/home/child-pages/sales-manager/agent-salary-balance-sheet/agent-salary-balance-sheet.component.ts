542d2b1480d3250dc4c57351884e3e8c637e60bc
sukantahui <huisukanta77@gmail.com>
2022-07-04 14:08:48 +0530

    database added

--- ready_made_angular/src/app/pages/home/child-pages/sales-manager/agent-salary-balance-sheet/agent-salary-balance-sheet.component.ts
+++ ready_made_angular/src/app/pages/home/child-pages/sales-manager/agent-salary-balance-sheet/agent-salary-balance-sheet.component.ts
@@ -13,7 +13,7 @@ export class AgentSalaryBalanceSheetComponent implements OnInit {
       console.log(response);
       // this.agents = response.agentSalaryBalanceSheetResolver.agents.data;
     });
-    
+
   }
 
   ngOnInit(): void {
