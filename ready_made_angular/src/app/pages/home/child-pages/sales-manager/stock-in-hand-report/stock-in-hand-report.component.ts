5f81662d2e2198586c6b49eafe3ac61ac5c57537
sukantahui <huisukanta77@gmail.com>
2022-09-30 13:48:06 +0530

    reference working

--- ready_made_angular/src/app/pages/home/child-pages/sales-manager/stock-in-hand-report/stock-in-hand-report.component.ts
+++ ready_made_angular/src/app/pages/home/child-pages/sales-manager/stock-in-hand-report/stock-in-hand-report.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {environment} from '../../../../../../environments/environment';
 import {ActivatedRoute} from '@angular/router';
 import {FormControl, FormGroup} from '@angular/forms';
+import {NgSelectComponent} from '@ng-select/ng-select';
 
 @Component({
   selector: 'app-stock-in-hand-report',
@@ -14,11 +15,13 @@ export class StockInHandReportComponent implements OnInit {
   agents: any[];
   productCategories: any[];
   reportForm: FormGroup;
+  stocksInHandFiltered: any;
   constructor(private route: ActivatedRoute) {
     this.route.data.subscribe((response: any) => {
       this.agents = response.showItemStockResolver.agents.data;
       this.productCategories = response.showItemStockResolver.productCategories.data;
       this.stocksInHand = response.showItemStockResolver.stocksInHand.data;
+      this.stocksInHandFiltered = response.showItemStockResolver.stocksInHand.data;
     });
 
     this.reportForm = new FormGroup({
@@ -30,7 +33,11 @@ export class StockInHandReportComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  stockByAgentAndCategory($event: any, value: any) {
+  stockByCategory($event: any, value: any) {
     console.log(value.selectedItems[0].value.ID);
+  }
+
+  stockByAgent($event: any, ref1: any) {
+
   }
 }
