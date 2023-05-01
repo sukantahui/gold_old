3900364d02366179310b80c417f46e4140340d86
sukantahui <huisukanta77@gmail.com>
2023-05-01 12:37:10 +0530

    lc received working

--- gold_api/app/Models/EmployeeCashBalance.php
+++ gold_api/app/Models/EmployeeCashBalance.php
@@ -10,11 +10,6 @@ class EmployeeCashBalance extends Model
     use HasFactory;
     protected $table = 'employees_cash_balance';
     protected $primaryKey = 'emp_id'; // or null
-<<<<<<< HEAD
     public $incrementing = false;
     public $timestamps = false;
-=======
-    public $timestamps = false;
-    public $incrementing = false;
->>>>>>> 37503e26134eaee644e9f13544f6dedef3517254
 }
