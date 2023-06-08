2d2c2abff9b3e23b8ff5f76a3e45f5dad9e83909
sukantahui <huisukanta77@gmail.com>
2023-06-08 14:07:20 +0530

    gold conversion saving

--- gold_api/app/Http/Controllers/MaterialTransformationMasterController.php
+++ gold_api/app/Http/Controllers/MaterialTransformationMasterController.php
@@ -35,13 +35,29 @@ class MaterialTransformationMasterController extends ApiController
             $mtm->karigar_id =$data->karigar_id;
             $mtm->save();
             $return_array['mtm']=$mtm;
-            $mtd=new MaterialTransformationDetail();
-            $mtd->mtm_id = $mtm->id;
-            $mtd->rm_id = $data->fine_gold_id;
-            $mtd->rm_value = $data->fine_gold_value;
-            $mtd->tr_type = -1;
-            $mtd->save();
-            $return_array['mtd']=$mtd;
+            $mtdFine=new MaterialTransformationDetail();
+            $mtdFine->mtm_id = $mtm->id;
+            $mtdFine->rm_id = $data->fine_gold_id;
+            $mtdFine->rm_value = $data->fine_gold_value;
+            $mtdFine->tr_type = -1;
+            $mtdFine->save();
+            $return_array['mtdFine']=$mtdFine;
+            $mtdCopper=new MaterialTransformationDetail();
+            $mtdCopper->mtm_id = $mtm->id;
+            $mtdCopper->rm_id = $data->copper_id;
+            $mtdCopper->rm_value = $data->copper_value;
+            $mtdCopper->tr_type = -1;
+            $mtdCopper->save();
+            $return_array['mtdCopper']=$mtdCopper;
+            //adding gini
+            $mtdGini=new MaterialTransformationDetail();
+            $mtdGini->mtm_id = $mtm->id;
+            $mtdGini->rm_id = $data->gini_id;
+            $mtdGini->rm_value = $data->gini_value;
+            $mtdGini->tr_type = 1;
+            $mtdGini->save();
+            $return_array['mtdGini']=$mtdGini;
+
             DB::commit();
         }catch (\Exception $e){
             DB::rollBack();
--- gold_api/app/Models/MaterialTransformationDetail.php
+++ gold_api/app/Models/MaterialTransformationDetail.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class MaterialTransformationDetail extends Model
 {
     use HasFactory;
+    private $mtm_id;
+    private $rm_id;
+    private $rm_value;
+    /**
+     * @var int
+     */
+    private $tr_type;
 }
