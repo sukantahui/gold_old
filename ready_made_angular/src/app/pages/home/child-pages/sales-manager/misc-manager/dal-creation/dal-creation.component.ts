4aa3ecadad9269726de72523e878de9a2460fae4
sukantahui <huisukanta77@gmail.com>
2023-06-20 13:59:49 +0530

    Dal creation material balance showing

--- ready_made_angular/src/app/pages/home/child-pages/sales-manager/misc-manager/dal-creation/dal-creation.component.ts
+++ ready_made_angular/src/app/pages/home/child-pages/sales-manager/misc-manager/dal-creation/dal-creation.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {environment} from '../../../../../../../environments/environment';
+import {ActivatedRoute} from '@angular/router';
+import {HttpClient} from '@angular/common/http';
+import {ManagerService} from '../../../../../../services/manager.service';
 
 @Component({
   selector: 'app-dal-creation',
@@ -8,8 +11,19 @@ import {environment} from '../../../../../../../environments/environment';
 })
 export class DalCreationComponent implements OnInit {
   isProduction = environment.production;
-    showDeveloperDiv = false;
-  constructor() { }
+  showDeveloperDiv = false;
+  karigars: any[];
+  user: any;
+  materialBalance: any[];
+  SavedResponse: any[];
+  constructor(private route: ActivatedRoute, private http: HttpClient, private managerService: ManagerService) {
+    this.route.data.subscribe((response: any) => {
+      this.materialBalance = response.fineToNinetyTwoResolver.materialBalance.data;
+      this.user = response.fineToNinetyTwoResolver.user;
+      this.karigars = response.fineToNinetyTwoResolver.karigars.data;
+      // console.log(response.fineToNinetyTwoResolver.user);
+    });
+  }
 
   ngOnInit(): void {
   }
