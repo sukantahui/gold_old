c75e0d6ee66763115a59ab69a9299f3bc7c7b26d
sukantahui <huisukanta77@gmail.com>
2024-12-11 14:19:59 +0530

    alert updated

--- ready_made_angular/src/app/pages/home/child-pages/sales-manager/Customer-Receipt/gold-received/gold-received.component.ts
+++ ready_made_angular/src/app/pages/home/child-pages/sales-manager/Customer-Receipt/gold-received/gold-received.component.ts
@@ -70,27 +70,55 @@ export class GoldReceivedComponent implements OnInit {
     });
   }
   saveGoldReceipt() {
-    this.receiptService.saveGoldReceipt(this.goldReceiptForm.value)
-        .subscribe((response: { status: any, data: any }) => {
-          if (response.status === true){
-            Swal.fire({
-              title: 'Done',
-              text: 'Gold Received added successfully',
-              icon: 'success'
+    Swal.fire({
+      title: 'Do you want to save the changes?',
+      showDenyButton: true,
+      showCancelButton: true,
+      confirmButtonText: 'Save',
+      denyButtonText: `Don't save`
+    }).then((result) => {
+      /* Read more about isConfirmed, isDenied below */
+      if (result.isConfirmed) {
+        // Saving gold received
+        this.receiptService.saveGoldReceipt(this.goldReceiptForm.value)
+            .subscribe((response: { status: any, data: any }) => {
+              if (response.status === true){
+                this.customerDues.lc_due  = this.customerDues.lc_due - this.goldReceiptForm.value.amount;
+                this.afterSaveResponse = response.data;
+                this.goldReceiptForm.markAsPristine();
+                this.goldReceipts.unshift(this.afterSaveResponse.gold_receipt);
+                this.goldReceiptForm.reset();
+                this.customerDues = null;
+
+                Swal.fire({
+                  title: '<strong>Gold Receipt Enter Successfully<u>example</u></strong>',
+                  icon: 'info',
+                  html: `You can use <b>bold text</b>,<a href="#" autofocus>links</a>, and other HTML tags`,
+                  showCloseButton: true,
+                  showCancelButton: true,
+                  focusConfirm: false,
+                  confirmButtonText: `<i class="fa fa-thumbs-up"></i> Great!`,
+                  confirmButtonAriaLabel: 'Thumbs up, great!',
+                  cancelButtonText: `<i class="fa fa-thumbs-down"></i>Cancel`,
+                  cancelButtonAriaLabel: 'Thumbs down'
+                });
+                // tslint:disable-next-line:no-unused-expression
+
+              }
+            }, (error) => {
+              Swal.fire({
+                title: 'Error Saving Gold Received',
+                text: 'Error saving Gold Receipt',
+                icon: 'error'
+              });
             });
-            // tslint:disable-next-line:no-unused-expression
-            this.customerDues.lc_due  = this.customerDues.lc_due - this.goldReceiptForm.value.amount;
-            this.afterSaveResponse = response.data;
-            this.goldReceiptForm.markAsPristine();
-            this.goldReceipts.unshift(this.afterSaveResponse.gold_receipt);
-          }
-        }, (error) => {
-          Swal.fire({
-            title: 'Done',
-            text: 'Error saving Gold Receipt',
-            icon: 'error'
-          });
-        });
+        // end of saving
+
+      } else if (result.isDenied) {
+        Swal.fire('Changes are not saved', '', 'info');
+      }
+    });
+
   }
 
   resetForm() {
